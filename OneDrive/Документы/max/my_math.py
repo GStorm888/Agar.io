2c45b5c7fa648ee2d0a8cf1de28b8ee88b1c7cda
GStorm888 <denchikevstr@icloud.com>
2024-04-27 21:14:14 +0300

    сделал рабочую функцию max

---
+++ OneDrive/Документы/max/my_math.py
@@ -0,0 +1,9 @@
+max_2_numbers = 0
+def max_2_numbers(first, second):
+    if first > second:
+        max_2_numbers = first
+    elif first < second:
+        max_2_numbers = second
+    else:
+        max_2_numbers = first
+
