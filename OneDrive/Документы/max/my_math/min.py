a3dbd3ef08cba14f32fe79fc1a2a23697a494493
GStorm888 <denchikevstr@icloud.com>
2024-04-27 21:16:21 +0300

    сделал функцию min

---
+++ OneDrive/Документы/max/my_math/min.py
@@ -0,0 +1,8 @@
+min_2_numbers = 0
+def max_2_numbers(first, second):
+    if first < second:
+        min_2_numbers = first
+    elif first < second:
+        min_2_numbers = second
+    else:
+        min_2_numbers = first
