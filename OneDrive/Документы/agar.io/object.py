7ab1ffeb034260ef14328a1548545cf8a0a209ce
GStorm888 <denchikevstr@icloud.com>
2024-06-26 10:21:21 +0300

    продолжаю написание класса object

--- OneDrive/Документы/agar.io/object.py
+++ OneDrive/Документы/agar.io/object.py
@@ -1,13 +1,30 @@
 import pygame
 import settings
 import random
+import settings
 
 class Object:
-    def __init__(self, x, y, mass, color):
+    def __init__(self, x, y, mass):
             self.x = x
             self.y = y
             self.mass = mass
-            self.color = color
+    def draw(self, centr):
+        self.mass = random.randint(settings.MIN_MASS, settings.MAX_MASS)
+        self.color = random.choice(settings.COLOR)
+        object_circle = pygame.draw.circle(centr, self.color, (self.x, self.y), self.mass)
 
-    def draw(self, win):
-        object_circle = pygame.draw.circle(win, self.color, (self.x, self.y), self.mass)
+class Player(Object):
+    def __init__(self, x, y, mass, color):
+         super().__init__(x, y, mass, color)
+
+    def draw(self, centr):
+        self.mass = 8
+        return super().draw(centr)
+    
+
+    def move():
+         ...
+
+
+    def eat():
+        ...
